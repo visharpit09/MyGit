780d1c7625710ebc6222a3af2b18754104dd910a
arpit_vishwakarma <arpit_vishwakarma.com>
2019-01-04 14:21:40 +0530

    prints All...git status

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWirld
 {
-public static void main (String [] args)
-{
-System.out.println("Hello Eorld.!!");
-}
+           public static void main (String [] args)
+       {
+           System.out.println("Hello Eorld...All...!!");
+       }
 }
